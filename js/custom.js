186bcb5064b067357902b3de94eec9ed3e1aacfe
stevemuir6 <68389769+stevemuir6@users.noreply.github.com>
2021-01-26 22:30:32 -0500

    first commit

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+
+// // reverse word input from input7 and display in output 7 and in reverse in output6
+
+document.getElementById("btnRevWord").addEventListener("click", function(){
+    let word2 = document.getElementById("input6").value;
+    
+     
+
+    let word3 = word2.split('').reverse().join('');
+    if (word2 == word3){
+        document.getElementById('output7').innerText= "The word you entered is a Palindrome!"
+    } else {
+        document.getElementById('output7').innerText ="The word you entered is not a Palindrome."
+    }
+});
+
+// clear output 7
+document.getElementById("btnClear2").addEventListener("click", function(){
+    document.getElementById("input6").value = "";
+    document.getElementById("output7").innerText = "";
+});
