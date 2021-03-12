1237f26f407ab4c3879a6c07f9f6ffda36444d16
stevemuir6 <68389769+stevemuir6@users.noreply.github.com>
2021-03-12 18:18:13 -0500

    updated sole page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,11 +6,16 @@
 document.getElementById("btnRevWord").addEventListener("click", function(){
     let word2 = document.getElementById("input6").value;
     
-     
+     // Non-word character regex
+     let reg = /[\W_]/g
 
-    let word3 = word2.split('').reverse().join('');
-    if (word2 == word3){
-        document.getElementById('output7').innerText= "The word you entered is a Palindrome!"
+     // convert input to lower case
+     var word3 = word2.toLowerCase().replace(reg, "")
+
+     // Reverse string
+    let word4 = word2.split('').reverse().join('');
+    if (word4 == word2){
+        document.getElementById('output7').innerText= `"${word2}" is a Palindrome!`
     } else {
         document.getElementById('output7').innerText ="The word you entered is not a Palindrome."
     }
